0887c7dc36adb68cf0c95249e9d0c7fc32066c1b
Khovanov <vagrant@rails-dev-box>
2016-10-04 13:42:11 +0000

    add localization

--- spec/acceptance/donations/edit_donation_spec.rb
+++ spec/acceptance/donations/edit_donation_spec.rb
@@ -5,14 +5,26 @@ feature 'Edit Donation', %q(
   I want to be able to edit donation
 ) do
 
+  given(:t_edit) { t('common.edit') }
+  given(:t_title) { t('activerecord.attributes.donation.title') }
+  given(:t_description) { t('activerecord.attributes.donation.description') }
+  given(:t_submit) { t('donations.form.submit', action: t('common.edit')) }
+  given(:t_title_eror) do
+    "#{t('activerecord.attributes.donation.title')}
+    #{t('activerecord.errors.messages.blank')}"
+  end
+  given(:t_description_eror) do
+    "#{t('activerecord.attributes.donation.description')}
+    #{t('activerecord.errors.messages.blank')}"
+  end
   given(:donation) { create :donation }
 
   scenario 'try to edit donation with valid params' do
     visit donation_path(donation)
-    click_on 'Изменить'
-    fill_in 'Заголовок', with: 'Edited donation title'
-    fill_in 'Описание', with: 'Edited donation description'
-    click_on 'Сохранить'
+    click_on t_edit
+    fill_in t_title, with: 'Edited donation title'
+    fill_in t_description, with: 'Edited donation description'
+    click_on t_submit
     expect(page).to have_content 'Edited donation title'
     expect(page).to have_content 'Edited donation description'
     expect(page).to_not have_content donation.title
@@ -21,11 +33,11 @@ feature 'Edit Donation', %q(
 
   scenario 'try to edit donation with invalid params' do
     visit donation_path(donation)
-    click_on 'Изменить'
-    fill_in 'Заголовок', with: nil
-    fill_in 'Описание', with: nil
-    click_on 'Сохранить'
-    expect(page).to have_content "Title can't be blank"
-    expect(page).to have_content "Description can't be blank"
+    click_on t_edit
+    fill_in t_title, with: nil
+    fill_in t_description, with: nil
+    click_on t_submit
+    expect(page).to have_content t_title_eror
+    expect(page).to have_content t_description_eror
   end
 end
--- spec/support/acceptance_macros.rb
+++ spec/support/acceptance_macros.rb
@@ -1,8 +1,8 @@
 module AcceptanceMacros
   def sign_in(user)
     visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+    fill_in t('activerecord.attributes.user.email'), with: user.email
+    fill_in t('activerecord.attributes.user.password'), with: user.password
+    click_on t('devise.sessions.new.sign_in')
   end
 end
