9dd7004ac9b3a879495bea8ff9fc44cd0d34b795
konstantinsmirnov <k.smi@mail.ru>
2016-10-22 20:16:46 +0300

    Replace confirmation field type

--- db/migrate/20161021192401_create_articles.rb
+++ db/migrate/20161021192401_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :content
+      t.text :content
     end
   end
 end
