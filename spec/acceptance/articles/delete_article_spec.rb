f2501504cafd0669119396873f4688edd2662028
konstantinsmirnov <k.smi@mail.ru>
2016-10-25 20:32:04 +0300

    Only registered users can create articles

--- spec/acceptance/articles/delete_article_spec.rb
+++ spec/acceptance/articles/delete_article_spec.rb
@@ -5,6 +5,9 @@ feature 'Delete Article', %q(
   I want to be able to delete article
 ) do
 
+  let(:user) { create(:user) }
+  before { sign_in user }
+
   given(:t_destroy) { t('common.destroy') }
   given(:article) { create :article }
 
@@ -14,5 +17,11 @@ feature 'Delete Article', %q(
       click_on t_destroy
     end
     expect(page).to_not have_content article.title
+  end
+
+  scenario 'not logged user can not delete article' do
+    sign_out user
+    visit article_path(article)
+    expect(page).to_not have_content t_destroy
   end
 end
