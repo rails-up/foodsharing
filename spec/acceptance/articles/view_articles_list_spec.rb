65253de936162b76a196ca55a6876b4842544f4f
konstantinsmirnov <k.smi@mail.ru>
2016-10-28 19:01:38 +0300

    Replace all articles with only published articles in ingex page

--- spec/acceptance/articles/view_articles_list_spec.rb
+++ spec/acceptance/articles/view_articles_list_spec.rb
@@ -1,11 +1,18 @@
 require_relative '../acceptance_helper'
 
 feature 'View Articles list', %q(
-  Any user can view list of articles
+  Any user can view list of published articles
 ) do
   given!(:articles) { create_list(:article, 3) }
+  scenario 'Any user can not view a list of not published articles' do
+    visit articles_path
+    articles.each do |article|
+      expect(page).to_not have_content article.title
+    end
+  end
 
-  scenario 'Any user can view a list of articles' do
+  scenario 'Any user can view a list of published articles' do
+    articles.each { |article| article.published! }
     visit articles_path
     articles.each do |article|
       expect(page).to have_content article.title
