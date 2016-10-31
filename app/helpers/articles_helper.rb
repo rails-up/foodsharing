3ae53a94c2066e6a2d6c33941e14426a1779beb0
konstantinsmirnov <k.smi@mail.ru>
2016-10-31 21:17:54 +0300

    Add helper for my_articles links

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,8 @@ module ArticlesHelper
   def count_articles(status)
     current_user.articles.where(status: status).count
   end
+
+  def link_to_my_articles(status)
+    link_to "#{t('.' + status.to_s)} [#{count_articles(status)}]", articles_path(my_articles: status), class: "btn-flat #{'disabled' if params[:my_articles] == status.to_s}"
+  end
 end
