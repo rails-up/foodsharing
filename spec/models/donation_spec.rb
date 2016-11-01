fbd2b20f39b4bfa77fa4b390ce94a84936cb4a91
Khovanov <vagrant@rails-dev-box>
2016-11-01 12:52:08 +0000

    spec for user and donation models

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Donation, type: :model do
+  describe 'associations for ...' do
+    it { should belong_to :user }
+  end
+
   describe 'validates presence of ...' do
     # expect(Donation.new(description: 'some description')).to_not be_valid
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
+    it { should validate_presence_of :user_id }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'associations for ...' do
+    it { should have_many(:donations).dependent(:destroy) }
+  end
+
   describe 'validates presence of ...' do
     it { should validate_presence_of :full_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
   end
 end
