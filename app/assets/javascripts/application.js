f2a6bef28bb48f793c892a5d88517ff9e91ac6d3
konstantinsmirnov <k.smi@mail.ru>
2016-11-05 13:37:05 +0300

    Add bootstrap and remove materialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require materialize-sprockets
 //= require jquery_ujs
+//= require tether
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
