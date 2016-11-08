b090cee87f17a9feecc563439c755b1979329b1e
Khovanov <vagrant@rails-dev-box>
2016-11-08 09:23:19 +0000

    add role model validation

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations for ...' do
+    it do
+      should have_and_belong_to_many(:users)
+        .join_table('users_roles')
+    end
+  end
 end
