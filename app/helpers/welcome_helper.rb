e39d7f4198235363d2ffef47ee42aa48142905cb
konstantinsmirnov <k.smi@mail.ru>
2016-11-08 21:30:03 +0300

    Do not show video on welcome in development env

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,7 @@
 module WelcomeHelper
+  def show_video
+    if Rails.env.production?
+      "{videoURL: 'https://www.youtube.com/watch?v=95SNbn340TE',containment:'.video-section', quality:'large', autoPlay:true, mute:true, opacity:1}"
+    end
+  end
 end
