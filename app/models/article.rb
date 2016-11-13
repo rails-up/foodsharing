75a2916221b14c8d60451f4292c9bd47dc2bee47
Khovanov <vagrant@rails-dev-box>
2016-11-13 14:42:54 +0000

    remove old role implementation

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ApplicationRecord
   resourcify
   include Authority::Abilities # updatable_by?
   belongs_to :user
+  enum status: { draft: 0, published: 1 }
   validates :title, :content, :user, presence: true
-  enum status: [:draft, :published]
 
   default_scope { order(updated_at: :desc) }
   scope :published, -> { where(status: :published).order(updated_at: :desc) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable, :omniauthable
+  # enum role: [:visitor, :editor]
+
   validates :full_name, presence: true
-  enum role: [:visitor, :editor]
   has_many :articles
   has_many :donations, dependent: :destroy
 end
