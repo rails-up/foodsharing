1191988b075f552f03e4c95dc23e30c3db128cf5
dshramko <da.shramko@gmail.com>
2016-11-15 10:22:17 +0600

    Add Company model and migrations

---
+++ db/migrate/20161115040822_create_companies.rb
@@ -0,0 +1,11 @@
+class CreateCompanies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :phone
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
