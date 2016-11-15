ca42b47336d5a387db4fbeea2db42ce8d8ecc2c3
Khovanov <vagrant@rails-dev-box>
2016-11-15 19:36:39 +0000

    add acceptance spec for view and view list special donations

--- spec/acceptance/donations/view_donation_spec.rb
+++ spec/acceptance/donations/view_donation_spec.rb
@@ -3,11 +3,71 @@ require_relative '../acceptance_helper'
 feature 'View Donation', %q(
   Any user can view donation
 ) do
-  given!(:donation) { create(:donation) }
+  given(:user) { create(:user) } # FactoryGirl.create
+  given(:cafe) { create(:user, role: :cafe) }
+  given(:volunteer) { create(:user, role: :volunteer) }
+  given(:editor) { create(:user, role: :editor) }
+  given(:admin) { create(:user, role: :admin) }
 
-  scenario 'Any user can view donation' do
-    visit donation_path(donation)
-    expect(page).to have_content donation.title
-    expect(page).to have_content donation.description
+  given(:donation) { create(:donation) }
+  given(:special_donation) { create(:donation, special: true) }
+
+  describe 'Unauthenticated user' do
+    scenario 'can view not special donation' do
+      visit donation_path(donation)
+      expect(page).to have_content donation.title
+      expect(page).to have_content donation.description
+    end
+
+    scenario 'can not view a special donation' do
+      visit donation_path(special_donation)
+      expect(page).to_not have_content special_donation.title
+      expect(page).to_not have_content special_donation.description
+    end
+  end
+
+  describe 'Authentitcated user without role' do
+    scenario 'can not view a special donation' do
+      sign_in user
+      visit donation_path(special_donation)
+      expect(page).to_not have_content special_donation.title
+      expect(page).to_not have_content special_donation.description
+    end
+  end
+
+  describe 'Authentitcated user with role :editor' do
+    scenario 'can not view a special donation' do
+      sign_in editor
+      visit donation_path(special_donation)
+      expect(page).to_not have_content special_donation.title
+      expect(page).to_not have_content special_donation.description
+    end
+  end
+
+  describe 'Authentitcated user with role :cafe' do
+    scenario 'can view a special donation' do
+      sign_in cafe
+      visit donation_path(special_donation)
+      expect(page).to have_content special_donation.title
+      expect(page).to have_content special_donation.description
+    end
+  end
+
+  describe 'Authentitcated user with role :volunteer' do
+    scenario 'can view a special donation' do
+      sign_in volunteer
+      visit donation_path(special_donation)
+      expect(page).to have_content special_donation.title
+      expect(page).to have_content special_donation.description
+    end
+  end
+
+  describe 'Authentitcated user with role :admin' do
+    scenario 'can view a special donation' do
+      sign_in admin
+      visit donation_path(special_donation)
+      expect(page).to have_content special_donation.title
+      expect(page).to have_content special_donation.description
+    end
   end
 end
