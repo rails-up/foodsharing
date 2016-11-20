972c965ef5214c30f4f15af4d488498e9bdd7056
dshramko <da.shramko@gmail.com>
2016-11-20 17:31:57 +0600

    Add page for editing of company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,21 +1,41 @@
 class CompaniesController < ApplicationController
+  before_action :load_user, only: [:new, :create]
+  before_action :load_company, only: [:edit, :update]
+
   def new
-    @user = User.find(current_user || session[:user_id])
     @company = @user.build_company
   end
 
   def create
-    @user = User.find(session[:user_id])
     @company = @user.build_company(companies_params)
     if @company.save
-      redirect_to root_path
+      redirect_to edit_user_registration_path
       session[:user_id] = nil
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @company.update(companies_params)
+      redirect_to edit_user_registration_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def load_user
+    @user = User.find(current_user || session[:user_id])
+  end
+
+  def load_company
+    @company = Company.find(params[:id])
+  end
 
   def companies_params
     params.require(:company).permit(:name, :phone, :address)
