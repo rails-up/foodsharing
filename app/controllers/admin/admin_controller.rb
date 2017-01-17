6a4eeafac56bd9f2ac1ae201a6c0eb8fb67411cb
dshramko <da.shramko@gmail.com>
2017-01-17 17:07:18 +0600

    Add check for admin role and authentication

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,11 +1,20 @@
 module Admin
-  class AdminController < ActionController::Base
+  class AdminController < ApplicationController
+    before_action :authenticate_user!
+    before_action :check_admin
     layout 'admin'
     protect_from_forgery with: :exception
 
     def dashboard
       @users_count = User.count
       @donations_count = Donation.count
+    end
+
+    private
+
+    def check_admin
+      flash[:notice] = I18n.t('alerts.access_denied')
+      redirect_to root_path unless current_user.has_role?(:admin)
     end
   end
 end
