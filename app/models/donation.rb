754116122c008f30bcfe0651f1779dee8f27889d
Ubuntu <ubuntu@rails-dev-box>
2017-01-28 23:52:00 +0300

    show date create for list donation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,4 +3,5 @@ class Donation < ApplicationRecord
   include Authority::Abilities # updatable_by?
   belongs_to :user
   validates :title, :description, :user_id, presence: true
+  default_scope { order(created_at: :desc) }
 end
