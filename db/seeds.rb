2cc5074c4626af2cb682754824a9f78b1099e10c
dshramko <da.shramko@gmail.com>
2017-02-03 10:05:13 +0600

    Add roles with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Role.destroy_all
+[:editor, :cafe, :volunteer, :admin].each do |role|
+  Role.create!(name: role)
+end
