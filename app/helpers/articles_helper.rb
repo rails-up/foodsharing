81c6a44707824b03d5c06144aac9cd5af897b5e2
dshramko <da.shramko@gmail.com>
2017-03-27 17:33:53 +0600

    Move date and time localization to separate file. Fix hound warnings

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -6,6 +6,7 @@ module ArticlesHelper
   def link_to_my_articles(status)
     link_to "#{t('.' + status.to_s)} [#{count_articles(status)}]",
             articles_path(my_articles: status),
-            class: "btn btn-outline-primary btn-sidebar #{'disabled' if params[:my_articles] == status.to_s}"
+            class: "btn btn-outline-primary btn-sidebar
+                    #{'disabled' if params[:my_articles] == status.to_s}"
   end
 end
